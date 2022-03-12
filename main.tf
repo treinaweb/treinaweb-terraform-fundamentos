c89bcc0d3559b25da0a67dce559e317d85a23901
Elton Fonseca <elton869@gmail.com>
2022-03-12 17:03:18 -0300

    13 - Conhecendo o comando state

--- main.tf
+++ main.tf
@@ -23,3 +23,12 @@ resource "aws_instance" "web_server" {
     Name = "ServidorSistemaWeb"
   }
 }
+
+resource "aws_instance" "web_server1" {
+  ami           = "ami-0792f512117871b0b"
+  instance_type = "t2.nano"
+
+  tags = {
+    Name = "ServidorSistemaWeb1"
+  }
+}
