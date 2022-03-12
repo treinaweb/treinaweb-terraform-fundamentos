af5bdaa35f13d13fabf3bb6d8f014d724ba8fa6e
Elton Fonseca <elton869@gmail.com>
2022-03-12 16:57:39 -0300

    12 - Alterando um recurso com Terraform

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 
 resource "aws_instance" "web_server" {
   ami           = "ami-0792f512117871b0b"
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
 
   tags = {
     Name = "ServidorSistemaWeb"
