3c771c9448cb783cf12e2aa662515749e39501dc
Elton Fonseca <elton869@gmail.com>
2022-03-12 15:30:09 -0300

    09 - Conhecendo o plano e execução

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0792f512117871b0b"
   instance_type = "t2.micro"
 
   tags = {
