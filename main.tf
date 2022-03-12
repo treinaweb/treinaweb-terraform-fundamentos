b0a3dbb0c32f9be894d8f609a01e682bd020de03
Elton Fonseca <elton869@gmail.com>
2022-03-12 13:53:28 -0300

    05 - Escolhendo e adicionando o provider no projeto

--- main.tf
+++ main.tf
@@ -2,3 +2,15 @@ output "ola_mundo" {
     value = "Olá Treinaweb!"
 }
 
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.5.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
